037585526efeb069c45b2ebd2d6ac24160d03361
Marvel.Codes <marvellousn561@gmail.com>
2024-05-29 01:25:51 +0100

    wip

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-// const hashEntry = require('./index');
 const cryptoHash =  require('./index')
 
 const email = 'marvel.code.craft@gmail.com';
 const password = "MyVeryOwnWalletAddress(01&1)";
-// const walletAddress = hashEntry(email, password);
+
 const walletAddressFromCrypto = cryptoHash(email, password)
 
 console.log(walletAddressFromCrypto);
